678260a650e1cbdd058a04c0e0c17d59a22559b2
Shoki11 <20cm0119@jec.ac.jp>
2021-12-10 10:15:31 +0900

    customCellの編集

--- LastApp/CustomCell/HairModelCell.swift
+++ LastApp/CustomCell/HairModelCell.swift
@@ -9,12 +9,18 @@ import UIKit
 
 class HairModelCell: UICollectionViewCell {
     
+    // MARK: - @IBOutlets
+    /// HairModelの画像を表示するImageView
     @IBOutlet weak var hairModelImageView: UIImageView!    
 
+    // MARK: - Mrthods
     override func awakeFromNib() {
         super.awakeFromNib()
     }
 
+    /// ShopCellを初期化する
+    /// - Parameters:
+    ///   - hairImage: HairModelの画像を表示する文字列
     func setUpHairModelCell(hairImage: String) {
         hairModelImageView.image = UIImage(named: hairImage)
     }
