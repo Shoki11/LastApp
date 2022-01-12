136a6fd13b2e913863e4f5443638d24a2542b759
Shoki11 <20cm0119@jec.ac.jp>
2022-01-12 16:26:40 +0900

    imageの追加

--- LastApp/ViewController.swift
+++ LastApp/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController {
     
     // MARK: - Propeties
     /// モデルの写真一覧を格納する配列
-    private let HairModelList = ["palette48","face48","back48", "rotation48", "palette24", "palette36"]
+    private let HairModelList = ["face", "men01", "face48", "back48", "rotation48", "palette36"]
     /// モデルの一覧を格納する配列
     private let HairModel = ["face", "men01", "women01"]
     /// 画面幅
